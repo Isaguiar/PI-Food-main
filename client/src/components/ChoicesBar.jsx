16dea80a27c0010397bd84fec3f5579a171edfea
Isaguiar <aguiara73@yahoo.com.br>
2021-11-09 21:46:20 -0300

    Version 9 11

--- client/src/components/ChoicesBar.jsx
+++ client/src/components/ChoicesBar.jsx
@@ -6,24 +6,16 @@ import {filterTypeChoices, filterOrigin} from '../utils/filterChoice.js';
 export default function ChoicesBar(){
 
 	const dispatch = useDispatch();
-	const recipes = useSelector(state=> state.recipes);
+	const recipes = useSelector(state=> state.allRecipes);
 	const types = useSelector(state=> state.dietTypes);
     
 
-	function handleOnClick(e){
+	function handleOnClick1(e){
 		e.preventDefault();
-		console.log(recipes.length);
-		console.log('value', e.target.value);
-		let result= [];
-		if(e.target.value === 'origin'){
-			result = recipes.filter(item => item[origin] === 'database');
-		} else{ 
-			result = recipes.filter(function(item){
+		let result = recipes.filter(function(item){
 			return item.diets.find(elem=> elem === e.target.value);
 		});
 
-		}
-		console.log(result.length);
 		if(!result.length){ 
 			alert('Ninguna receta encontrada!')
 	    } else{
@@ -31,11 +23,29 @@ export default function ChoicesBar(){
 	    }
 	}
 
+	function handleOnClick2(e){
+		e.preventDefault();
+		console.log(recipes[100].origin, recipes[0].origin)
+		let result = recipes.filter(item => item.origin ==='database');
+		if(!result.length){ 
+			alert('Ninguna receta encontrada!')
+	    } else{
+	        dispatch(updateList(result));
+	    }		
+	}
+
+	function handleOnClick3(e){
+		e.preventDefault();
+		dispatch(updateList(recipes));
+	}
+
+
 	return(
 	<>
 	<div>
-	{types?.map(item=> {return <button type= "button" value= {item.name} key={item.id} onClick={handleOnClick}>{item.name} </button>})}
-	<button type = 'button' key='invented' value= 'origin' handleOnClick ={handleOnClick}>Sugerencias de los usuarios</button>
+	{types?.map(item=> {return <button type= "button" value= {item.name} key={item.id} onClick={handleOnClick1}>{item.name} </button>})}
+	<button type = 'button' key='invented' value= 'origin' handleOnClick ={handleOnClick2}>Sugerencias de los usuarios</button>
+	<button type = 'button' key='inventedtoo' value= 'todas' handleOnClick ={handleOnClick2}>todas</button>
 	</div>
 	</>)
 }
--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -2,17 +2,16 @@ import {Link} from 'react-router-dom';
 import SortList from './SortList.jsx';
 import SearchBar from './SearchBar.jsx';
 import{findByName} from '../actions';
-//import handleSelectOrder from '../utils/handleSelectOrder/js'
+
 
 export default function NavBar(){
 
-	
 	return (
 		<>
-		 <Link to = 'recipes/home'><span>HOME</span></Link>
-         <Link to = 'recipes/add'><span>CREAR UNA RECETA</span></Link>
+		 <Link to = '/recipes/home'><span>HOME</span></Link>
+         <Link to = '/recipes/add'><span>Sugira una receta...</span></Link>
          <span><SearchBar /></span>
-         <span><SortList /></span>
          <span><button>Buscar</button></span>
+         <span><SortList /></span>
 		</>)
 }

21f6eb626953390fccf3cf357c699bd514ec0528
Isaguiar <aguiara73@yahoo.com.br>
2022-01-14 17:48:01 -0300

    Solve warnings navBar file

--- client/src/components/navBar/index.jsx
+++ client/src/components/navBar/index.jsx
@@ -2,7 +2,6 @@ import styles from './NavBar.module.css';
 import {Link} from 'react-router-dom';
 import SortList from '../sortList';
 import SearchBar from '../searchBar';
-import{findByName} from '../../actions';
 
 
 export default function NavBar(){
